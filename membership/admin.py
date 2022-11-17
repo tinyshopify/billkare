b4e01b809ea101d576e4e2940ba2505c986e84ee
nandhini <nandhini28nec@gmail.com>
2022-11-17 16:55:41 +0530

    mainchanges

--- membership/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Subscription,SuganSubscriptionlookup
-# Register your models here.
-admin.site.register(Subscription)
-admin.site.register(SuganSubscriptionlookup)
